553efcbb65acd815a464a4b991b9d1c66a70cf91
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2019-03-19 14:45:51 +0100

    allow to use time domain inputs

--- model.py
+++ model.py
@@ -1,23 +1,79 @@
-from torch.nn import Module, LSTM, Linear, InstanceNorm1d, Parameter
-import torch.nn.functional as F
+from torch.nn import LSTM, Linear, InstanceNorm1d, Parameter
 import torch
-import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class OSU(Module):
+class Spectrogram(nn.Module):
     def __init__(
-        self, nb_features, nb_frames,
-        nb_channels=1, nb_layers=3, hidden_size=512,
-        output_mean=None,
+        self, 
+        n_fft=2048, 
+        n_hop=1024, 
+        window=torch.hann_window, 
+        power=1, 
+        mono=True
+    ):
+
+        super(Spectrogram, self).__init__()
+
+        self.window = nn.Parameter(
+            window(n_fft),
+            requires_grad=False
+        )
+        self.n_fft = n_fft
+        self.n_hop = n_hop
+        self.power = power
+        self.mono = mono
+
+    def forward(self, x):
+        """
+        Input: (nb_samples, nb_channels, nb_timesamples)
+        Output:(nb_frames, nb_samples, nb_channels, nb_bins)
+        """
+        assert x.dim() == 3
+
+        nb_samples, nb_channels, _ = x.size()
+
+        # merge nb_samples and nb_channels for multichannel stft
+        x = x.reshape(nb_samples*nb_channels, -1)
+
+        # compute stft with parameters as close as possible scipy settings
+        stft_f = torch.stft(
+            x,
+            n_fft=self.n_fft, hop_length=self.n_hop,
+            window=self.window, center=True,
+            normalized=False, onesided=True,
+            pad_mode='reflect'
+        ).transpose(1, 2)
+
+        stft_f = stft_f.pow(2).sum(-1).pow(self.power / 2.0)
+        stft_f = stft_f.contiguous().view(nb_samples, nb_channels, -1, self.n_fft // 2 + 1)
+
+        if self.mono:
+            stft_f = torch.sqrt(torch.sum(stft_f**2, 1, keepdim=True))
+        
+        return stft_f.permute(2, 0, 1, 3)
+
+
+class OSU(nn.Module):
+    def __init__(
+        self, 
+        n_fft=2048,
+        n_hop=1024,
+        power=1,
+        nb_channels=1, 
+        nb_layers=3, 
+        hidden_size=512,
     ):
         super(OSU, self).__init__()
 
         self.hidden_size = hidden_size
-
-        self.in0 = InstanceNorm1d(nb_features*nb_channels)
+        self.nb_bins = n_fft // 2 + 1
+        self.spec = Spectrogram(n_fft=n_fft, n_hop=n_hop, power=1, mono=(nb_channels == 1))
+        self.in0 = InstanceNorm1d(self.nb_bins*nb_channels)
 
         self.fc1 = Linear(
-            nb_features*nb_channels, hidden_size,
+            self.nb_bins*nb_channels, hidden_size,
             bias=False
         )
 
@@ -41,30 +97,33 @@ class OSU(Module):
 
         self.fc3 = Linear(
             in_features=hidden_size,
-            out_features=nb_features*nb_channels,
+            out_features=self.nb_bins*nb_channels,
             bias=False
         )
 
         self.in3 = InstanceNorm1d(hidden_size)
 
-        self.output_scale = Parameter(
-            torch.ones(nb_features).float()
-        )
-
-        self.output_mean = Parameter(
-            torch.from_numpy(np.copy(output_mean)).float()
+        self.fc4 = Linear(
+            in_features=self.nb_bins*nb_channels,
+            out_features=self.nb_bins*nb_channels,
+            bias=True
         )
 
     def forward(self, x):
-        nb_frames, nb_batches, nb_channels, nb_features = x.data.shape
+        # transform input to spectrogram
+
+        # check for waveform or image
+        x = self.spec(x)
+        nb_frames, nb_batches, nb_channels, nb_bins = x.data.shape
+
         # shift and scale input to mean=0 std=1 (across all bins)
-        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_features)
+        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_bins)
         x = self.in0(x.permute(1, 2, 0)).permute(2, 0, 1)
 
         # reduce feature dimensions, therefore we reshape
-        # to (nb_frames*nb_batches, nb_channels*nb_features)
+        # to (nb_frames*nb_batches, nb_channels*nb_bins)
         # and encode to (nb_frames*nb_batches, hidden_size)
-        x = self.fc1(x.reshape(-1, nb_channels*nb_features))
+        x = self.fc1(x.reshape(-1, nb_channels*nb_bins))
         x = x.reshape(nb_frames, nb_batches, self.hidden_size)
         # normalize every instance in a batch
         x = self.in1(x.permute(1, 2, 0)).permute(2, 0, 1)
@@ -87,17 +146,16 @@ class OSU(Module):
 
         # second dense stage + layer norm
         x = self.fc3(x.reshape(-1, self.hidden_size))
-        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_features)
+        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_bins)
         x = self.in3(x.permute(1, 2, 0)).permute(2, 0, 1)
+        
+        # scale back to output domain
+        x = self.fc4(x.reshape(-1, nb_channels*nb_bins))
 
         # reshape back to sequence
-        x = x.reshape(nb_frames, nb_batches, nb_channels, nb_features)
-
-        # apply output scaling
-        x *= self.output_scale
-        x += self.output_mean
+        x = x.reshape(nb_frames, nb_batches, nb_channels, nb_bins)
 
         # since our output is non-negative, we can apply RELU
         x = F.relu(x)
-
+        
         return x
