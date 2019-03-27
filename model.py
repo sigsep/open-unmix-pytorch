99d74eb06bfd42b438132a3f5c609730bb5b8771
Fabian-Robert Stöter <mail@faroit.com>
2019-03-27 21:58:41 +0100

    change wording of samples/timesteps

--- model.py
+++ model.py
@@ -28,11 +28,11 @@ class STFT(nn.Module):
 
     def forward(self, x):
         """
-        Input: (nb_samples, nb_channels, nb_timesamples)
+        Input: (nb_samples, nb_channels, nb_timesteps)
         Output:(nb_samples, nb_channels, nb_bins, nb_frames, 2)
         """
 
-        nb_samples, nb_channels, nb_timesamples = x.size()
+        nb_samples, nb_channels, nb_timesteps = x.size()
 
         # merge nb_samples and nb_channels for multichannel stft
         x = x.reshape(nb_samples*nb_channels, -1)
@@ -75,7 +75,9 @@ class Spectrogram(nn.Module):
 
         # downmix in the mag domain
         if self.mono:
-            stft_f = torch.sqrt(torch.sum(stft_f**2, 1, keepdim=True))
+            stft_f = torch.sum(
+                stft_f**2/self.power, 1, keepdim=True
+            ) ** (self.power/2)
 
         # permute output for LSTM convenience
         return stft_f.permute(2, 0, 1, 3)
@@ -92,6 +94,11 @@ class OSU(nn.Module):
         hidden_size=512,
         image=False
     ):
+        """
+        Input: (nb_samples, nb_channels, nb_timesteps) or (nb_samples, nb_bins, nb_frames, 2)
+        Output: Power/Mag Spectrogram (nb_frames, nb_samples, nb_channels, nb_bins)
+        """
+
         super(OSU, self).__init__()
 
         self.hidden_size = hidden_size
@@ -142,13 +149,13 @@ class OSU(nn.Module):
 
     def forward(self, x):
         # check for waveform or image
-        # transform to spectrogram if (nb_batches, nb_channels, samples)
+        # transform to spectrogram if (nb_samples, nb_channels, nb_timesteps)
         x = self.transform(x)
 
-        nb_frames, nb_batches, nb_channels, nb_bins = x.data.shape
+        nb_frames, nb_samples, nb_channels, nb_bins = x.data.shape
 
         # shift and scale input to mean=0 std=1 (across all bins)
-        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_bins)
+        x = x.reshape(nb_frames, nb_samples, nb_channels*nb_bins)
 
         # apply manual instance "normalization"
         mean = x.mean(0, keepdim=True)
@@ -158,10 +165,10 @@ class OSU(nn.Module):
         # x = self.in0(x.permute(1, 2, 0)).permute(2, 0, 1)
 
         # reduce feature dimensions, therefore we reshape
-        # to (nb_frames*nb_batches, nb_channels*nb_bins)
-        # and encode to (nb_frames*nb_batches, hidden_size)
+        # to (nb_frames*nb_samples, nb_channels*nb_bins)
+        # and encode to (nb_frames*nb_samples, hidden_size)
         x = self.fc1(x.reshape(-1, nb_channels*nb_bins))
-        x = x.reshape(nb_frames, nb_batches, self.hidden_size)
+        x = x.reshape(nb_frames, nb_samples, self.hidden_size)
         # normalize every instance in a batch
         x = self.in1(x.permute(1, 2, 0)).permute(2, 0, 1)
         # squash range ot [-1, 1]
@@ -176,25 +183,25 @@ class OSU(nn.Module):
 
         # first dense stage + batch norm
         x = self.fc2(x.reshape(-1, self.hidden_size))
-        x = x.reshape(nb_frames, nb_batches, self.hidden_size)
+        x = x.reshape(nb_frames, nb_samples, self.hidden_size)
         x = self.in2(x.permute(1, 2, 0)).permute(2, 0, 1)
 
         x = F.relu(x)
 
         # second dense stage + layer norm
         x = self.fc3(x.reshape(-1, self.hidden_size))
-        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_bins)
+        x = x.reshape(nb_frames, nb_samples, nb_channels*nb_bins)
         x = self.in3(x.permute(1, 2, 0)).permute(2, 0, 1)
 
         # reshape back to sequence
-        x = x.reshape(nb_frames, nb_batches, nb_channels*nb_bins)
+        x = x.reshape(nb_frames, nb_samples, nb_channels*nb_bins)
 
         # scale back to output
         x *= stddev
         x += mean
 
         # reshape back to sequence
-        x = x.reshape(nb_frames, nb_batches, nb_channels, nb_bins)
+        x = x.reshape(nb_frames, nb_samples, nb_channels, nb_bins)
 
         # add learnable scale
         x *= self.output_scale
