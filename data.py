d2a6778c10bea63d3028afc06691d20083487b86
Fabian-Robert Stöter <mail@faroit.com>
2019-03-27 22:05:26 +0100

    even more samples

--- data.py
+++ data.py
@@ -96,7 +96,7 @@ class MUSDBDataset(torch.utils.data.Dataset):
         target='vocals',
         seq_duration=None,
         validation_split='train',
-        samples_per_track=16,
+        samples_per_track=64,
         *args, **kwargs
     ):
         """MUSDB18 Dataset wrapper
