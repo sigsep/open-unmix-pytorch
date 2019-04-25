c59352d661953b8b87f2d0467ab5208306ca9a66
Fabian-Robert Stöter <mail@faroit.com>
2019-04-26 00:48:03 +0200

    make cleaner SourcesDataset

--- data.py
+++ data.py
@@ -14,6 +14,12 @@ except ImportError:
     musdb = None
 
 
+def random_product(*args, repeat=1):
+    "Random selection from itertools.product(*args, **kwds)"
+    pools = [tuple(pool) for pool in args] * repeat
+    return tuple(random.choice(pool) for pool in pools)
+
+
 class SourcesDataset(torch.utils.data.Dataset):
     def __init__(
         self,
@@ -21,8 +27,10 @@ class SourcesDataset(torch.utils.data.Dataset):
         seq_duration=None,
         input_sources_paths=['noises', 'vocals'],
         output_source_path='vocals',
+        ext="*.wav",
         sample_rate=44100,
-        nb_samples=1000
+        nb_samples=1000,
+        paired=True
     ):
         """A dataset of that assumes folders with unmatched sources
         generating input mixtures matched to a target
@@ -43,14 +51,21 @@ class SourcesDataset(torch.utils.data.Dataset):
         # convert sequence duration into samples as torchaudio is samplebased
         if seq_duration is not None:
             self.seq_duration = int(seq_duration * sample_rate)
+        else:
+            self.seq_duration = None
         self.nb_samples = nb_samples
-        self.samples = list(self.get_track_paths())
+        self.ext = ext
+        self.input_sources_paths = input_sources_paths
+        self.output_source_path = output_source_path
+        self.sources_samples = self.get_track_paths()
 
     def __getitem__(self, index):
-        input_path, output_path = self.samples[index]
-        X_audio = self.load_audio(input_path)
-        Y_audio = self.load_audio(output_path)
-        return X_audio, Y_audio
+        input_tuple = random_product(*self.sources_samples)
+        sources = list(map(self.load_audio, input_tuple))
+        mix = torch.stack(sources, dim=0).sum(dim=0)
+        ind = self.input_sources_paths.index(self.output_source_path)
+        target = self.load_audio(input_tuple[ind])
+        return mix, target
 
     def __len__(self):
         return self.nb_samples
@@ -78,14 +93,13 @@ class SourcesDataset(torch.utils.data.Dataset):
 
     def get_track_paths(self):
         """Loads input and output tracks"""
-        p = Path(self.root)
-        for track_folder in p.iterdir():
-            if track_folder.is_dir():
-                input_path = list(track_folder.glob(self.targets['x']))
-                output_path = list(track_folder.glob(self.targets['y']))
-                # if both targets are available in the subfolder add them
-                if input_path and output_path:
-                    yield input_path[0], output_path[0]
+
+        sources_paths = []
+        for sources_folder in self.input_sources_paths:
+            p = Path(self.root, sources_folder)
+            sources_paths.append(list(p.glob(self.ext)))
+
+        return sources_paths
 
 
 class TargetsDataset(torch.utils.data.Dataset):
