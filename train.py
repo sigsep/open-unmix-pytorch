small mistake in validation loss averaging

train.py
@@ -134,7 +134,7 @@ optimizer = optim.Adam(unmix.parameters(), lr=args.lr)
 criterion = torch.nn.MSELoss()
 
 
-def train(epoch):
+def train():
     losses = utils.AverageMeter()
     unmix.train()
 
@@ -152,7 +152,6 @@ def train(epoch):
 
 def valid():
     losses = utils.AverageMeter()
-
     unmix.eval()
     with torch.no_grad():
         for x, y in valid_sampler:
@@ -160,7 +159,7 @@ def valid():
             Y_hat = unmix(x)
             Y = unmix.transform(y)
             loss = F.mse_loss(Y_hat, Y)
-            losses.update(loss.item(), x.size(1))
+            losses.update(loss.item(), Y.size(1))
         return losses.avg
 
 
@@ -172,7 +171,7 @@ valid_losses = []
 train_times = []
 for epoch in t:
     end = time.time()
-    train_loss = train(epoch)
+    train_loss = train()
     valid_loss = valid()
     train_losses.append(train_loss)
     valid_losses.append(valid_loss)