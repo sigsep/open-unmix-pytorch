de2eba7abb9d456b4cbeca6a81fdc617c5859b96
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2019-07-09 19:45:29 +0200

    update urls

--- hubconf.py
+++ hubconf.py
@@ -21,10 +21,10 @@ def umxhq(
     """
     # set urls for weights
     target_urls = {
-        'bass': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/bass-e3e1c4be.pth',
-        'drums': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/drums-a849ec7d.pth',
-        'other': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/other-732546c5.pth',
-        'vocals': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/vocals-f3d5fab0.pth'
+        'bass': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX-HQ/bass-e3e1c4be.pth',
+        'drums': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX-HQ/drums-a849ec7d.pth',
+        'other': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX-HQ/other-732546c5.pth',
+        'vocals': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX-HQ/vocals-f3d5fab0.pth'
     }
 
     # determine the maximum bin count for a 16khz bandwidth model
@@ -71,10 +71,10 @@ def umx(
     """
     # set urls for weights
     target_urls = {
-        'bass': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/bass-e3e1c4be.pth',
-        'drums': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/drums-a849ec7d.pth',
-        'other': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/other-732546c5.pth',
-        'vocals': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/OpenUnmix-1.0/vocals-f3d5fab0.pth'
+        'bass': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX/bass-cf6c8240.pth',
+        'drums': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX/drums-6f6173f5.pth',
+        'other': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX/other-0f2ff5cc.pth',
+        'vocals': 'https://open-unmix.s3.eu-west-3.amazonaws.com/pytorch-models/UMX/vocals-fbc27976.pth'
     }
 
     # determine the maximum bin count for a 16khz bandwidth model
