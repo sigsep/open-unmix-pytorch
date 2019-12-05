59f10be72652690fe707b0e469c59aaffbb41df2
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2019-12-05 16:25:54 +0100

    implement inference

--- test.py
+++ test.py
@@ -15,35 +15,20 @@ from contextlib import redirect_stderr
 import io
 
 
-def load_model(target, model_name='umxhq', device='cpu'):
+def load_model(targets, model_name='umxhq', device='cpu'):
     """
     target model path can be either <target>.pth, or <target>-sha256.pth
     (as used on torchub)
     """
     model_path = Path(model_name).expanduser()
     if not model_path.exists():
-        # model path does not exist, use hubconf model
-        try:
-            # disable progress bar
-            err = io.StringIO()
-            with redirect_stderr(err):
-                return torch.hub.load(
-                    'sigsep/open-unmix-pytorch',
-                    model_name,
-                    target=target,
-                    device=device,
-                    pretrained=True
-                )
-            print(err.getvalue())
-        except AttributeError:
-            raise NameError('Model does not exist on torchhub')
-            # assume model is a path to a local model_name direcotry
+        raise NotImplementedError
     else:
         # load model from disk
-        with open(Path(model_path, target + '.json'), 'r') as stream:
+        with open(Path(model_path, len(targets) + '.json'), 'r') as stream:
             results = json.load(stream)
 
-        target_model_path = next(Path(model_path).glob("%s*.pth" % target))
+        target_model_path = Path(model_path) / "model.pth"
         state = torch.load(
             target_model_path,
             map_location=device
@@ -55,7 +40,8 @@ def load_model(target, model_name='umxhq', device='cpu'):
             results['args']['bandwidth']
         )
 
-        unmix = model.OpenUnmix(
+        unmix = model.OpenUnmixJoint(
+            targets=results['args']['targets'],
             n_fft=results['args']['nfft'],
             n_hop=results['args']['nhop'],
             nb_channels=results['args']['nb_channels'],
@@ -134,25 +120,17 @@ def separate(
     audio_torch = torch.tensor(audio.T[None, ...]).float().to(device)
 
     source_names = []
-    V = []
 
-    for j, target in enumerate(tqdm.tqdm(targets)):
-        unmix_target = load_model(
-            target=target,
-            model_name=model_name,
-            device=device
-        )
-        Vj = unmix_target(audio_torch).cpu().detach().numpy()
-        if softmask:
-            # only exponentiate the model if we use softmask
-            Vj = Vj**alpha
-        # output is nb_frames, nb_samples, nb_channels, nb_bins
-        V.append(Vj[:, 0, ...])  # remove sample dim
-        source_names += [target]
+    unmix = load_model(
+        targets=targets,
+        model_name=model_name,
+        device=device
+    )
+    V = unmix(audio_torch).cpu().detach().numpy()
 
     V = np.transpose(np.array(V), (1, 3, 2, 0))
 
-    X = unmix_target.stft(audio_torch).detach().cpu().numpy()
+    X = unmix.stft(audio_torch).detach().cpu().numpy()
     # convert to complex numpy type
     X = X[..., 0] + X[..., 1]*1j
     X = X[0].transpose(2, 1, 0)
@@ -169,8 +147,8 @@ def separate(
     for j, name in enumerate(source_names):
         audio_hat = istft(
             Y[..., j].T,
-            n_fft=unmix_target.stft.n_fft,
-            n_hopsize=unmix_target.stft.n_hop
+            n_fft=unmix.stft.n_fft,
+            n_hopsize=unmix.stft.n_hop
         )
         estimates[name] = audio_hat.T
 
