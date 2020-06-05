26113b7db200bd6611f3e928281d1cb777589527
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2020-06-05 15:10:21 +0200

    centering doesn"t seem work with torchaudio.istft

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -3,18 +3,27 @@ import numpy as np
 import torch
 import model
 import test
+import torchaudio
 
-def istft(X, rate=44100, n_fft=4096, n_hopsize=1024,
-          center=False):
-    from torchaudio.functional import istft
-    return istft(
+
+def istft(
+    X,
+    rate=44100,
+    n_fft=4096,
+    n_hopsize=1024,
+    center=False
+):
+    return torchaudio.functional.istft(
         X,
         n_fft=n_fft,
         hop_length=n_hopsize,
         window=torch.hann_window(n_fft),
         center=center,
-        normalized=False, onesided=True,
-        pad_mode='reflect')
+        normalized=False,
+        onesided=True,
+        pad_mode='reflect'
+    )
+
 
 @pytest.fixture(params=[4096, 4096*10])
 def nb_timesteps(request):
@@ -26,7 +35,7 @@ def nb_channels(request):
     return request.param
 
 
-@pytest.fixture(params=[1, 2, 16])
+@pytest.fixture(params=[1, 2])
 def nb_samples(request):
     return request.param
 
@@ -46,12 +55,24 @@ def audio(request, nb_samples, nb_channels, nb_timesteps):
     return torch.rand((nb_samples, nb_channels, nb_timesteps))
 
 
-def test_stft(audio, nb_channels, nfft, hop):
+@pytest.mark.parametrize(
+    ("center"),
+    [
+        True,
+        pytest.param(
+            False,
+            marks=pytest.mark.xfail(
+                reason="https://github.com/pytorch/audio/issues/500"
+            )
+        ),
+    ],
+)
+def test_stft(audio, nb_channels, nfft, hop, center):
     unmix = model.OpenUnmix(nb_channels=nb_channels)
-    unmix.stft.center = True
+    unmix.stft.center = center
     X = unmix.stft(audio)
     X = X.detach()
-    out = istft(X, center=unmix.stft.center)
+    out = istft(X, center=center)
     assert np.sqrt(
         np.mean((audio.detach().numpy() - out.detach().numpy())**2)
     ) < 1e-6
