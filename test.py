c4527de1a932e6d84f1abb32503f74e43442cacf
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2020-06-18 16:20:04 +0200

    update test module, remove soundfile

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import torch
 import argparse
-import soundfile as sf
+import torchaudio
 from pathlib import Path
 import model
 from model import Separator
@@ -24,6 +24,13 @@ def inference_args(parser, remaining_args):
     )
 
     inf_parser.add_argument(
+        '--wiener-win-len',
+        type=int,
+        default=300,
+        help='Number of frames on which to apply filtering independently'
+    )
+
+    inf_parser.add_argument(
         '--residual',
         type=str,
         default=None,
@@ -36,9 +43,9 @@ def inference_args(parser, remaining_args):
         type=str,
         default=None,
         help='if provided, must be a string containing a valid expression for '
-             'a dictionary, with keys the output target names, and values '
+             'a dictionary, with keys as output target names, and values '
              'a list of targets that are used to build it. For instance: '
-             '\'{\"vocals\":[\"vocals\"], \'accompaniment\':[\"drums\",'
+             '\'{\"vocals\":[\"vocals\"], \"accompaniment\":[\"drums\",'
              '\"bass\",\"other\"]}\''
     )
     return inf_parser.parse_args()
@@ -104,28 +111,24 @@ if __name__ == '__main__':
     # parsing the output dict
     out = None if args.out is None else json.loads(args.out)
 
-    separator = Separator(targets=targets,
-                          niter=args.niter,
-                          residual=args.residual,
-                          out=out,
-                          wiener_win_len=300).to(device)
+    separator = Separator(
+        targets=targets,
+        niter=args.niter,
+        residual=args.residual,
+        wiener_win_len=args.wiener_win_len
+    ).to(device)
     separator.freeze()
 
     # loop over the files
     for input_file in args.input:
         # handling an input audio path
-        audio, rate = sf.read(input_file, always_2d=True)
-
-        # convert numpy audio to torch
-        audio_torch = torch.as_tensor(audio).to(device)
-        audio_torch = utils.preprocess(audio, rate, separator.sample_rate)
+        audio, rate = torchaudio.load(input_file)
+        audio = audio.to(device)
+        audio = utils.preprocess(audio, rate, separator.sample_rate)
 
         # getting the separated signals
-        estimates = separator(audio_torch)
-
-        estimates = {
-            key: estimates[key][0].detach().cpu().numpy()
-            for key in estimates}
+        estimates = separator(audio)
+        estimates = separator.to_dict(estimates, aggregate_dict=out)
 
         if not args.outdir:
             model_path = Path(args.model)
@@ -139,8 +142,9 @@ if __name__ == '__main__':
 
         # write out estimates
         for target, estimate in estimates.items():
-            sf.write(
-                outdir / Path(target).with_suffix('.wav'),
-                estimate,
-                separator.sample_rate
+            print(type(estimate))
+            torchaudio.save(
+                 str(outdir / Path(target).with_suffix('.wav')),
+                 torch.squeeze(estimate),
+                 separator.sample_rate
             )
