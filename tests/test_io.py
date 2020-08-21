ea0b12bb5a7e22998be763aa08b3ae51454629b2
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2020-08-21 20:23:18 +0200

    add io unit test to check duration. Also add functionality to discard info loading if already done in preprocessing

---
+++ tests/test_io.py
@@ -0,0 +1,38 @@
+import pytest
+import numpy as np
+import data
+import os
+import data
+
+
+audio_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'data/test.wav',
+)
+
+
+@pytest.fixture(params=["sox", "soundfile", "sox_io"])
+def torch_backend(request):
+    return request.param
+
+
+@pytest.fixture(params=[1.0, 2.0, None])
+def dur(request):
+    return request.param
+
+
+@pytest.fixture(params=[True, False])
+def info(request):
+    if request.param:
+        return data.load_info(audio_path)
+    else:
+        return None
+
+
+def test_loadwav(dur, info):
+    audio = data.load_audio(audio_path, dur=dur, info=info)
+    rate = 8000
+    if dur:
+        assert audio.shape[-1] == int(dur * rate)
+    else:
+        assert audio.shape[-1] == rate * 3
