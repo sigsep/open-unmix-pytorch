b226140e76788cd758678a712c432a584a5b0c84
Antoine Liutkus <antoine.liutkus@inria.fr>
2020-10-15 12:38:26 +0200

    removed predict altogether to add a functional interface in __init__ directly

--- openunmix/cli.py
+++ openunmix/cli.py
@@ -10,10 +10,11 @@ import argparse
 
 
 def unmix():
-    # Training settings
     parser = argparse.ArgumentParser(
         description='UMX Inference',
-        add_help=False
+        add_help=True,
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter
     )
 
     parser.add_argument(
@@ -72,7 +73,40 @@ def unmix():
         help='Set torchaudio backend '
              '(`sox` or `soundfile`), defaults to `sox`')
 
-    args = predict.inference_args(parser)
+    parser.add_argument(
+        '--niter',
+        type=int,
+        default=1,
+        help='number of iterations for refining results.'
+    )
+
+    parser.add_argument(
+        '--wiener-win-len',
+        type=int,
+        default=300,
+        help='Number of frames on which to apply filtering independently'
+    )
+
+    parser.add_argument(
+        '--residual',
+        type=str,
+        default=None,
+        help='if provided, build a source with given name'
+             'for the mix minus all estimated targets'
+    )
+
+    parser.add_argument(
+        '--aggregate',
+        type=str,
+        default=None,
+        help='if provided, must be a string containing a valid expression for '
+             'a dictionary, with keys as output target names, and values '
+             'a list of targets that are used to build it. For instance: '
+             '\'{\"vocals\":[\"vocals\"], \"accompaniment\":[\"drums\",'
+             '\"bass\",\"other\"]}\''
+    )
+
+    args = parser.parse_args()
 
     torchaudio.set_audio_backend(args.audio_backend)
 
