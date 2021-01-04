21c623232ccdc20b962915b555085acf92e79492
Fabian-Robert Stöter <mail@faroit.com>
2021-01-04 18:05:07 +0100

    reduce version

--- scripts/exports/model_to_tf.py
+++ scripts/exports/model_to_tf.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import json
 from pathlib import Path
-from openunmix import model
+import openunmix 
 from openunmix import utils
 
 import warnings
@@ -16,42 +16,65 @@ pp = pprint.PrettyPrinter(indent=4)
 import tensorflow as tf
 
 
-def apply_remove_weight_norm(module):
-    try:
-        torch.nn.utils.remove_weight_norm(module)
-    except ValueError:  # this module didn't have weight norm
-        return
-
 
 if __name__ == "__main__":
+    umx = openunmix.umxhq_spec(device="cpu")
 
-    umx = utils.load_target_models(
-        targets="vocals",
-        model_str_or_path="umxhq",
-        device="cpu",
-    )['vocals']
+    for target in ['vocals', 'drums', 'bass', 'other']:
+        x = torch.randn(
+            (1, 2, 2049, 128),
+            requires_grad=True
+        )
+        umx_t = umx[target]
+        out = umx_t(x)
 
-    x = torch.randn(
-        (1, 2, 2049, 100),
-        requires_grad=True
-    )
-    torch_out = umx(x)
-    # Export the model
-    # using nb_frames, nb_samples, nb_channels, nb_bins
-    torch.onnx.export(
-        umx,                 # model being run
-        x,                   # model input (or a tuple for multiple inputs)
-        "vocals.onnx",
-        export_params=True,  # store the trained parameter weights inside the model file
-        opset_version=9,     # the ONNX version to export the model to
-        do_constant_folding=True,  # whether to execute constant folding for optimization
-        input_names=['mix'],       # the model's input names
-        output_names=['vocals'],   # the model's output names
-        verbose=True,
-        # dynamic_axes={'mix': {3: 'sequence'}, 'vocals': {3: 'sequence'}}
-    )
+        # Export the model
+        # using nb_frames, nb_samples, nb_channels, nb_bins
+        torch.onnx.export(
+            umx_t,                 # model being run
+            x,                   # model input (or a tuple for multiple inputs)
+            target + ".onnx",
+            export_params=True,  # store the trained parameter weights inside the model file
+            opset_version=9,     # the ONNX version to export the model to
+            do_constant_folding=True,  # whether to execute constant folding for optimization
+            input_names=['mix'],       # the model's input names
+            output_names=[target],   # the model's output names
+            verbose=True
+        )
 
-    onnx_model = onnx.load("vocals.onnx")  # load onnx model
-    print(onnx_model.graph.input[0])
-    tf_rep = prepare(onnx_model)  # prepare tf representation
-    tf_rep.export_graph("vocals-savedmodel")  # export the model
+        onnx_model = onnx.load(target + ".onnx")  # load onnx model
+        print(onnx_model.graph.input[0])
+        # onnx_model.graph.input[0].type.tensor_type.shape.dim[3].dim_value = -1
+        tf_rep = prepare(onnx_model)  # prepare tf representation
+        tf_rep.export_graph(target + ".pb")  # export the model
+
+    umx = openunmix.umxse_spec(device="cpu")
+
+    for target in ['speech']:
+        x = torch.randn(
+            (1, 1, 513, 128),
+            requires_grad=True
+        )
+        umx_t = umx[target]
+        out = umx_t(x)
+
+        # Export the model
+        # using nb_frames, nb_samples, nb_channels, nb_bins
+        torch.onnx.export(
+            umx_t,                 # model being run
+            x,                   # model input (or a tuple for multiple inputs)
+            target + ".onnx",
+            export_params=True,  # store the trained parameter weights inside the model file
+            opset_version=9,     # the ONNX version to export the model to
+            do_constant_folding=True,  # whether to execute constant folding for optimization
+            input_names=['mix'],       # the model's input names
+            output_names=[target],   # the model's output names
+            # dynamic_axes={'mix': {3: 'sequence'}, target: {3: 'sequence'}},
+            verbose=True
+        )
+
+        onnx_model = onnx.load(target + ".onnx")  # load onnx model
+        print(onnx_model.graph.input[0])
+        # onnx_model.graph.input[0].type.tensor_type.shape.dim[3].dim_value = -1
+        tf_rep = prepare(onnx_model)  # prepare tf representation
+        tf_rep.export_graph(target + ".pb")  # export the model
